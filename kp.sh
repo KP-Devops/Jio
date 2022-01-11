ecdaed26bbb3bd83caf2493f8cb4c1a7d3fa2c4a
KP <krishnaprasad1287@gmail.com>
2022-01-11 19:42:06 +0530

    added loc to check commit and push in one cmd

--- kp.sh
+++ kp.sh
@@ -1,1 +1,3 @@
-echo "This is a test file for kp in shell script just to display the message" 
+echo "This is a test file for kp in shell script just to display the message"
+
+echo "Added this line for second commit and push in GIT HUB"
