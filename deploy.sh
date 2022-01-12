ac9b905dec29fe2ce4feba12a058c2a93ac4567e
KP <krishnaprasad1287@gmail.com>
2022-01-12 22:10:14 +0530

    reverting the 3 files back Revert "added files"
    This reverts commit 2f9967e9798403b6c7b5ebfb4a060e51348ffe95.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-added this loc to test the merging.....
-
 hi this file contains deployment logic
 
 echo "To commit and stage"
--- file1.sh
+++
@@ -1,7 +0,0 @@
-To test the reset command created this file 
-
-once you add the code from working are to staging area, use git reset command to get back the files staging area to working area.
-
-echo "all set"
-
-
