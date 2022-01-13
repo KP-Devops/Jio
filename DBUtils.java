e48cd958a3d6ae3cf6219efd2b382fcff90a08d7
KP <krishnaprasad1287@gmail.com>
2022-01-13 22:44:40 +0530

    test1

--- DBUtils.java
+++ DBUtils.java
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-Iam updating in dev branch ......
-=======
-Hello Iam updating this line on 13th jan to test the cherry-pick 
-
->>>>>>> e324ec3 (to test the cherry pick)
 Iam updating in master branch
 
 hi this is a test file for jave in git repoisitory. 
