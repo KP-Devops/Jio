a1e38311efecd3dca967b26f46605b9ac476d9c5
KrShasidar <96781615+KrShasidar@users.noreply.github.com>
2022-01-17 23:43:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,6 @@ hi this file contains deployment logic
 echo "To commit and stage"
 
 This line is added in UAT branch to demonstrate the PR (Pull Request)
+
+
+This line is added to know the  working of the CLone command for the Fetch............. 
